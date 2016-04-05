48807512879b2547388b4fc2d6de8bd9aac5b85f
Artem Zinoviev <arzinoviev@gmail.com>
2016-04-05 12:54:20 +0300

    add video selection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBePHgkFwHENEihj0gKJ1ndv636aK6sn8M';
 
@@ -10,10 +11,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
     YTSearch({ key: API_KEY, term: 'crossfit girls' }, (videos) => {
-      this.setState({ videos }); // sugar for: { videos: videos }
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -21,7 +28,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
